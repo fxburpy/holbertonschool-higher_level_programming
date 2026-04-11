847ea470b9ce6ed884246b79d91d7114769716a7
fxburpy <13808holbertonstudents.com>
2026-04-11 19:54:41 +0000

    fix pycodestyle

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+nano 1-last_digit.py#!/usr/bin/python3
 import random
 
 number = random.randint(-10000, 10000)
@@ -15,4 +15,3 @@ elif last_digit == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
-
