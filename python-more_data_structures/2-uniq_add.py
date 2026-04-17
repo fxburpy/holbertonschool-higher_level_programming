ca7ad9903b050797a531a0a2a83aabf7861d1883
fxburpy <13808@holbertonstudents.com>
2026-04-17 17:21:56 +0400

    Create 2-uniq_add.py

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+def uniq_add(my_list=[]):
+    unique_numbers = []
+    total = 0
+
+    for num in my_list:
+        if num not in unique_numbers:
+            unique_numbers.append(num)
+            total += num
+
+    return total
