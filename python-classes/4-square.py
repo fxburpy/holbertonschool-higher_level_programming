2f280b5f0ea59dd9e8ad18352698cb4357b7a850
fxburpy <13808@holbertonstudents.com>
2026-04-25 01:55:24 +0400

    Update 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,9 +2,11 @@
 class Square:
     def __init__(self,size=0):
         self.size=size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self,value):
         if type(value)is not int:
@@ -12,5 +14,6 @@ class Square:
         if value<0:
             raise ValueError("size must be >= 0")
         self.__size=value
+
     def area(self):
-        return self.__size**2
+        return self.__size*self.__size
