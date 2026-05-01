23fcc73b893048b49394e37c8adcf028a7b9b640
fxburpy <13808@holbertonstudents.com>
2026-05-01 23:57:53 +0400

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Module that reads a text file (UTF8) and prints it to stdout.
+"""
+
 
 def read_file(filename=""):
+    """
+    Reads a file and prints its content to stdout.
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
