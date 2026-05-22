bd6607e0189926dff8ed86dc22f9d231d5a8284a
fxburpy <13808@holbertonstudents.com>
2026-05-22 11:31:30 +0400

    Create 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines BaseGeometry class."""
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+
+    def area(self):
+        """Raise an exception because area is not implemented."""
+        raise Exception("area() is not implemented")
