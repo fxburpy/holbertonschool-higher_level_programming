41d2686b661eb7c4254cb142f61c61dc3fe3d795
fxburpy <13808@holbertonstudents.com>
2026-06-19 23:50:37 +0400

    Update 0-basic_async_syntax.py

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""Module that provides an asynchronous coroutine using the random module."""
+"""Module that provides an asynchronous coroutine using random."""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Waits for a random delay between 0 and max_delay seconds and returns it."""
+    """Waits for a random delay up to max_delay and returns it."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
