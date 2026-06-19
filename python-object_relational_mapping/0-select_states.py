f0f76c02bce835beae6d261229f82bea9f53bed4
fxburpy <13808@holbertonstudents.com>
2026-06-19 22:34:18 +0400

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa."""
+
 import MySQLdb
 import sys
 
@@ -14,8 +16,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    rows = cur.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
 
     cur.close()
