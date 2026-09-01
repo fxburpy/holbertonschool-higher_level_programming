f4e757a81b066fd0ecc600ac0c923fe5267749fa
fxburpy <13808@holbertonstudents.com>
2026-09-01 20:32:38 +0400

    Make state prefix filter case-sensitive

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC"
     )
 
     rows = cursor.fetchall()
