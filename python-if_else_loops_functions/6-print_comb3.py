8095ddd3f40562edf63dcd68f1b00402380d9bae
fxburpy <13808@holbertonstudents.com>
2026-09-01 18:58:55 +0400

    Print unique two digit combinations

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j))
+for first_digit in range(10):
+    for second_digit in range(first_digit + 1, 10):
+        if first_digit == 8 and second_digit == 9:
+            print("{}{}".format(first_digit, second_digit))
         else:
-            print("{:d}{:d}".format(i, j), end=", ")
+            print("{}{}, ".format(first_digit, second_digit), end="")
