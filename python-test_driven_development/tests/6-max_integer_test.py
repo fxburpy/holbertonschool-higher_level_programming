2a5affc1dd0e386b7cf89b7a7070d3da6b3810c5
fxburpy <13808@holbertonstudents.com>
2026-09-01 19:32:43 +0400

    Expand test-driven development edge cases

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -14,6 +14,9 @@ class TestMaxInteger(unittest.TestCase):
     def test_unordered_integers(self):
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
+    def test_maximum_at_beginning(self):
+        self.assertEqual(max_integer([9, 3, 4, 2]), 9)
+
     def test_negative_integers(self):
         self.assertEqual(max_integer([-4, -2, -9]), -2)
 
@@ -28,6 +31,9 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_floats(self):
         self.assertEqual(max_integer([1.5, 4.2, 3.8]), 4.2)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([1, 7.5, -3, 7]), 7.5)
 
     def test_strings(self):
         self.assertEqual(max_integer(["apple", "pear", "banana"]), "pear")
